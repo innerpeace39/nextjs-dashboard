d9669c23e96b472304b618d2e597dbe44249ec16
innerpeace39 <gopinathmep.39@gmail.com>
2025-05-18 20:08:03 +0530

    subset added

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,4 +4,4 @@ export const inter = Inter({ subsets: ['latin'] });
 
 import { Lusitana } from 'next/font/google';
  
-export const lusitana = Lusitana({weight: "400"});
+export const lusitana = Lusitana({weight: "400", subsets: ['latin']});
